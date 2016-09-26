576ca33af0b6e7c89cf9f4de235fa7ddc5b42561
init <arakawa_keisuke@tokai-grp.co.jp>
2016-09-26 16:22:14 +0900

    test

---
+++ www/js/home/slideMenu.js
@@ -0,0 +1,8 @@
+// This is a JavaScript file
+function openSlideMenu() {
+    document.getElementById("slideMenu").style.width = "100px";
+}
+
+function closeSlideMenu() {
+    document.getElementById("slideMenu").style.width = "0";
+}
